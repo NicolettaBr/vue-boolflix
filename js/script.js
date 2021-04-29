cf1476d7f20a04561e25eb05fa0ec9df7c49c129
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-04-29 17:47:31 +0200

    scheletro html/css + stampa info film e serie tv

--- js/script.js
+++ js/script.js
@@ -2,30 +2,59 @@ var app = new Vue(
     {
         el: '#root',
         data: {
-            userResearch: 'vita',
-            arrayMovies: []
+            userResearch: '',
+            arrayMovies: [],
+            arraySeries:[]
         },
 
         methods:{
+            search(){
+               if(this.userResearch.length > 0){
+                   /*chiamata dei film*/
+                axios
+                .get('https://api.themoviedb.org/3/search/movie', {
+                    params:{
+                        api_key: 'b24d6d28719609dba95ff636cc59a0e3',
+                        query: this.userResearch,
+                    }
+                })
+                .then((response)=>{
+                   //console.log(response);
+                   const result = response.data;
+                   //console.log(result);
+                   //console.log(result.results);
+                   this.arrayMovies = result.results;
+                   console.log(this.arrayMovies);
+                })
+
+                /*chiamata delle serieTV*/
+                axios
+                .get('https://api.themoviedb.org/3/search/tv', {
+                    params:{
+                        api_key: 'b24d6d28719609dba95ff636cc59a0e3',
+                        query: this.userResearch,
+                    }
+                })
+                .then((response)=>{
+                   //console.log(response);
+                   const result = response.data;
+                   //console.log(result);
+                   //console.log(result.results);
+                   this.arraySeries = result.results;
+                   console.log(this.arraySeries);
+                })
+                
+                /*resetta barra input*/ 
+                this.userResearch = '';
+               }
+
+                
+            }
 
         },
 
         mounted(){
-            axios
-            .get('https://api.themoviedb.org/3/search/movie', {
-                params:{
-                    api_key: 'b24d6d28719609dba95ff636cc59a0e3',
-                    query: this.userResearch,
-                }
-            })
-            .then((response)=>{
-               //console.log(response);
-               const result = response.data;
-               //console.log(result);
-               //console.log(result.results);
-               this.arrayMovies = result.results;
-               console.log(this.arrayMovies);
-            })
+           
           
 
         }
